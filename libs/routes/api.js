96ccfb1f56e0059704b8eff57e2228a467f76834
Marco Gautier <marco.gautier@epitech.eu>
2019-04-25 23:41:07 +0200

    fix(API): add status to get on api route

--- libs/routes/api.js
+++ libs/routes/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 var router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json({
+    res.status(200).json({
         msg: 'API is up'
     });
 });
