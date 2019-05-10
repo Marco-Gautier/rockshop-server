8103d080b2a49d9556af8f7cf9b4a741033e61e4
Marco Gautier <marco.gautier@epitech.eu>
2019-05-10 02:20:03 +0200

    fix(http): Add headers to response (CORS)

--- libs/app.js
+++ libs/app.js
@@ -15,6 +15,13 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE, OPTIONS");
+    next();
+});
+
 app.use('/', api);
 app.use('/api', api);
 app.use('/api/user', user);
