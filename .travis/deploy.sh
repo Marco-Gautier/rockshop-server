898fd6bfc6de5b7333a37fd9bfd2f659873b48b0
Marco GAUTIER <marco.gautier@epitech.eu>
2019-06-17 04:06:06 +0200

    fix(deployment): fix error in ssh comand in deployment script

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -5,4 +5,4 @@ git config --global push.default simple # we only want to push one branch — ma
 # <user> here is the separate user you created for deploying
 git remote add production ssh://marco@51.77.202.182:4242/home/marco/apps/rockshop-server-deploy
 git push production master # push our updates
-ssh ssh marco@51.77.202.182 -p 4242 "cd apps/rockshop-server-deploy && docker-compose up --build"
+ssh marco@51.77.202.182 -p 4242 "cd apps/rockshop-server-deploy && docker-compose up --build &" exit
